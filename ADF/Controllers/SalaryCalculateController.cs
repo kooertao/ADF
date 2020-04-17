763fce2fb75a20ae5c1e941fd4aab5bd53c29763
lhe <heliang_neu@163.com>
2020-04-17 18:21:31 +0800

    First Commit

---
+++ ADF/Controllers/SalaryCalculateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaryCalculateController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetTotalSalary(int year)
+        {
+            return Ok($"{year} total income:100w");
+        }
+    }
+}
